b1dfd0716fd4bbb2e7a2074a8334c9aaf56e1b0e
Vamsi Munagala <Vamsi.Munagala@hotmail.com>
2024-01-30 10:34:57 -0500

    Challenge2 - attempt1

--- scripts/challenge1.sh
+++ scripts/challenge1.sh
@@ -1,8 +1,13 @@
+rg="aml-rg"
+wp="amlwp"
+aml_cluster_name="aml-cluster"
+
 az extension add -n ml
-az ml data create --file ../experimentation/dataset.yml --workspace-name vlm_ml_example --resource-group vlm_x_002
+az ml data create --file dataset.yml --workspace-name $wp --resource-group $rg
 
 #Create a compute cluster
-#az ml compute create --name myCompute --size Standard_DS3_v2 --type aml-cluster --workspace-name vlm_ml_example --resource-group vlm_x_002
+az ml compute create --file amlcompute.yml --workspace-name $wp --resource-group $rg
+#az ml compute create --name $aml_cluster_name --size Standard_DS3_v2 --type aml-cluster --workspace-name $wp --resource-group $rg
 
 #Create a Job
-az ml job create --file ../src/job.yml --workspace-name vlm_ml_example --resource-group vlm_x_002 
+az ml job create --file ../src/job.yml --workspace-name $wp --resource-group $rg 
