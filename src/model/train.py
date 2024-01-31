08d04213c53237cea72159024d56f6f05c86b63a
Vamsi Munagala <Vamsi.Munagala@hotmail.com>
2024-01-31 09:56:52 -0500

    challeneg4 - attempt3

--- src/model/train.py
+++ src/model/train.py
@@ -35,19 +35,25 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+# function to split data
 def split_data(df):
-    X = df[['Pregnancies', 
-            'PlasmaGlucose', 
-            'DiastolicBloodPressure', 
-            'TricepsThickness', 
-            'SerumInsulin', 
-            'BMI', 
-            'DiabetesPedigree', 
-            'Age']].values
-    y = df['Diabetic'].values
+    X = df[
+        [
+            "Pregnancies",
+            "PlasmaGlucose",
+            "DiastolicBloodPressure",
+            "TricepsThickness",
+            "SerumInsulin",
+            "BMI",
+            "DiabetesPedigree",
+            "Age",
+        ]
+    ].values
+    y = df["Diabetic"].values
     print(len(X))
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
     return X_train, X_test, y_train, y_test
 
 
