1fb0acf41c26e78d4c8c7397e3e33a5a3c1b2992
Vamsi Munagala <Vamsi.Munagala@hotmail.com>
2024-01-31 09:46:16 -0500

    challenge4 - linting

--- src/model/train.py
+++ src/model/train.py
@@ -4,10 +4,10 @@ import argparse
 import glob
 import os
 import mlflow
-import pathlib
+#import pathlib
 
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -39,11 +39,12 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values
+    y = df['Diabetic'].values
     print(len(X))
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
-    return X_train, X_test, y_train, y_test 
-    
+    return X_train, X_test, y_train, y_test
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -65,6 +66,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
